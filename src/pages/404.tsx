e81f47ae34df73f506b2d1326979add2d0d95bb0
Tony DiPasquale <anthony.r.dipasquale@gmail.com>
2021-02-26 15:10:36 -0800

    Add 404

---
+++ src/pages/404.tsx
@@ -0,0 +1,22 @@
+import DefaultLayout from '@layouts/Default';
+import { getAlbums } from '@api';
+
+export default function NotFound(props) {
+  return (
+    <DefaultLayout albums={props.albums}>
+      <section className="flex-1 mt-8">
+        <h1 className="text-4xl text-center">Page Not Found</h1>
+      </section>
+    </DefaultLayout>
+  );
+}
+
+export async function getStaticProps() {
+  const albums = await getAlbums();
+
+  return {
+    props: {
+      albums,
+    }
+  };
+}
