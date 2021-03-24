33ee1e8a622e4c9f80395e8e26c62af92098961c
Tony DiPasquale <anthony.r.dipasquale@gmail.com>
2021-03-24 12:20:13 -0700

    Build sass manually

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,8 @@
-const sass = require("sass");
-const fs = require("fs");
-const postcss = require('postcss');
 const format = require('date-fns/format');
 const site = require('./_data/site.json');
 
 module.exports = config => {
   config.addPassthroughCopy({ "public": "/" });
-  config.on('afterBuild', renderSass);
-
-  if (process.env.ELEVENTY_ENV !== 'production') {
-    config.addWatchTarget("./_sass");
-  }
 
   config.addFilter('dateForSitemap', function (date) {
     return format(date, 'yyyy-MM-dd');
@@ -27,17 +19,3 @@ module.exports = config => {
     }
   };
 };
-
-function renderSass() {
-  sass.render({file: "_sass/main.scss"}, function (err, result) {
-    if (err) return console.error(err);
-
-    postcss([require('tailwindcss'), require('autoprefixer')]).process(result.css.toString(), { from: '_sass/main.css', to: 'css/main.css' })
-      .then( function (result) {
-        fs.writeFileSync("_site/css/main.css", result.css, function () { return true; });
-        if (result.map) {
-          fs.writeFileSync("_site/css/main.css.map", result.map.toString(), function () { return true; });
-        }
-      });
-  })
-}
