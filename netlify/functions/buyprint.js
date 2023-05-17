f7bba79389ba202285746e50fe47e3797d8464ed
Tony DiPasquale <anthony.r.dipasquale@gmail.com>
2023-05-16 17:38:02 -0700

    Add function to buy photo

---
+++ netlify/functions/buyprint.js
@@ -0,0 +1,32 @@
+const stripe = require('stripe')(process.ENV.STRIPE_API_KEY);
+
+export.handler = async function (event, context) {
+  const session = await stripe.checkout.sessions.create({
+    line_items: [
+      {
+        price_data: {
+          currency: 'USD',
+          product_data: {
+            name: '11x14 Print',
+            images: [ event.queryStringParameters.image_url ],
+            metadata: {
+              filename: event.queryStringParameters.filename
+            },
+            tax_code: 'txcd_20090028'
+          },
+          unit_amount: '10',
+          tax_behavior: 'exclusive'
+        },
+        quantity: 1
+      }
+    ],
+    mode: 'payment',
+    success_url: 'index.html'
+  });
+
+  return {
+    statusCode: 303,
+    headers: {
+      'Location': session.url
+  };
+};
