f35d089d18b3c214e263ce2ea7e6856e9c942a1a
Tony DiPasquale <anthony.r.dipasquale@gmail.com>
2023-05-18 11:42:49 -0700

    Add error handling

--- netlify/edge-functions/transform-thankyou.js
+++ netlify/edge-functions/transform-thankyou.js
@@ -2,31 +2,37 @@ import Stripe from 'https://cdn.jsdelivr.net/npm/stripe@12.5.0/+esm';
 
 
 export default async (request, context) => {
-  const stripe = new Stripe(Deno.env.get("STRIPE_API_KEY"));
+  try {
+    const stripe = new Stripe(Deno.env.get("STRIPE_API_KEY"));
 
-  const url = new URL(request.url);
-  if (!url.searchParams.get("session_id")) {
+    const url = new URL(request.url);
+    if (!url.searchParams.get("session_id")) {
+      const url = new URL("/", request.url);
+      return Response.redirect(url);
+    }
+
+    const session = await stripe.checkout.sessions.retrieve(url.searchParams.get("session_id"));
+    const customer = session["customer_details"];
+    const lineItems = await stripe.checkout.sessions.listLineItems(url.searchParams.get("session_id"));
+    const product = await stripe.products.retrieve(lineItems.data[0].price.product);
+
+    const response = await context.next();
+    const page = await response.text();
+
+    const imageRegex = /IMAGE_URL/i;
+    const productRegex = /PRODUCT_NAME/i;
+    const customerRegex = /CUSTOMER_NAME/i;
+
+    const imagePage = page.replace(imageRegex, product.images[0]);
+    const productPage = imagePage.replace(productRegex, product.name);
+    const updatedPage = productPage.replace(customerRegex, customer.name);
+
+    return new Response(updatedPage, response);
+  } catch (e) {
+    console.error(e);
     const url = new URL("/", request.url);
     return Response.redirect(url);
   }
-
-  const session = await stripe.checkout.sessions.retrieve(url.searchParams.get("session_id"));
-  const customer = session["customer_details"];
-  const lineItems = await stripe.checkout.sessions.listLineItems(url.searchParams.get("session_id"));
-  const product = await stripe.products.retrieve(lineItems.data[0].price.product);
-
-  const response = await context.next();
-  const page = await response.text();
-
-  const imageRegex = /IMAGE_URL/i;
-  const productRegex = /PRODUCT_NAME/i;
-  const customerRegex = /CUSTOMER_NAME/i;
-
-  const imagePage = page.replace(imageRegex, product.images[0]);
-  const productPage = imagePage.replace(productRegex, product.name);
-  const updatedPage = productPage.replace(customerRegex, customer.name);
-
-  return new Response(updatedPage, response);
 }
 
 export const config = { path: "/thankyou/" };
