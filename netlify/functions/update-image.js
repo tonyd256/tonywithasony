bccdf2eff9ffdefcfd6fd1f46163d0e774a0ae1b
Tony DiPasquale <anthony.r.dipasquale@gmail.com>
2024-09-10 14:17:34 -0700

    Fixup function

--- netlify/functions/update-image.js
+++ netlify/functions/update-image.js
@@ -3,19 +3,20 @@ require('dotenv').config();
 
 export default async (req, context) => {
   try {
-    console.dir(req, { depth: null });
-    console.dir(context, { depth: null });
-    if (req.body.Type === "SubscriptionConfirmation") {
+    const data = JSON.parse(req.body);
+    console.dir(data, { depth: null });
+
+    if (req.headers.get("x-amz-sns-message-type") === "SubscriptionConfirmation") {
       const client = new SNSClient({
         credentials: {
-          accessKeyId: process.env.SNS_AWS_ACCESS_KEY_ID,
-          secretAccessKey: process.env.SNS_AWS_SECRET_ACCESS_KEY
+          accessKeyId: Netlify.env.get("SNS_AWS_ACCESS_KEY_ID"),
+          secretAccessKey: Netlify.env.get("SNS_AWS_SECRET_ACCESS_KEY")
         },
         region: "us-west-2"
       });
       const options = {
-        TopicArn: req.headers['x-amz-sns-topic-arn'],
-        Token: req.body.Token
+        TopicArn: req.headers.get('x-amz-sns-topic-arn'),
+        Token: data.Token
       };
 
       const result = await client.send(new ConfirmSubscriptionCommand(options));
