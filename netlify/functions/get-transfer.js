04e632a231b40d0cbba98736f62d88f5aa259e2c
Tony DiPasquale <anthony.r.dipasquale@gmail.com>
2025-02-16 16:18:46 -0800

    Add a zip file

--- netlify/functions/get-transfer.js
+++ netlify/functions/get-transfer.js
@@ -1,4 +1,5 @@
-const { S3Client, ListObjectsV2Command } = require("@aws-sdk/client-s3");
+const { S3Client, ListObjectsV2Command, GetObjectCommand } = require("@aws-sdk/client-s3");
+const { getSignedUrl } = require('@aws-sdk/s3-request-presigner');
 const _ = require('lodash');
 require('dotenv').config();
 
@@ -24,7 +25,16 @@ async function handler(req, context) {
 
     const cmd = new ListObjectsV2Command(params);
     const { Contents } = await s3Client.send(cmd);
-    const vals = _.map(_.map(Contents, "Key"), newImage);
+    const vals = await Promise.all(_.map(_.map(Contents, "Key"), async function (key) {
+      const cmd = new GetObjectCommand({ Bucket: process.env.B2_BUCKET_NAME, Key: key, ResponseContentDisposition: `attachment; filename="${key.split('/').pop()}"` });
+      const url = await getSignedUrl(s3Client, cmd, { expiresIn: 60*60 });
+
+      if (key.endsWith('.zip')) {
+        return { name: key, url, mime: 'application/zip' };
+      } else {
+        return { name: key, url, image_url: new URL("tony/"+key, process.env.IMAGEKIT_URL_ENDPOINT).href, mime: "image/jpeg" };
+      }
+    }));
     return { body: JSON.stringify(vals), headers: { "Content-Type": "application/json" }, statusCode: 200 };
   } catch (err) {
     console.error('Error getting images:', err);
@@ -33,11 +43,6 @@ async function handler(req, context) {
 }
 
 function newImage(name) {
-  return {
-    name: name,
-    url:  new URL("tony/"+name, process.env.IMAGEKIT_URL_ENDPOINT).href,
-    mime: "image/jpeg"
-  };
 }
 
 exports.handler = handler;
