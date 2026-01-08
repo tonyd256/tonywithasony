dfe0ea8cab30777dc40bf267076566f3ca26375f
Tony DiPasquale <anthony.r.dipasquale@gmail.com>
2026-01-07 16:17:35 -0800

    It's app key

--- netlify/functions/sftpgo-to-cloudinary.js
+++ netlify/functions/sftpgo-to-cloudinary.js
@@ -48,7 +48,7 @@ exports.handler = async (event) => {
       endpoint: process.env.B2_ENDPOINT, // e.g. "https://s3.us-west-004.backblazeb2.com"
       credentials: {
         accessKeyId: process.env.B2_KEY_ID,
-        secretAccessKey: process.env.B2_APPLICATION_KEY,
+        secretAccessKey: process.env.B2_APP_KEY,
       },
       forcePathStyle: true, // helps with some S3-compatible providers
     });
